2805f506a83f182dcdd62a7dbfd0cbe2f7f5e8a8
dmartinez8198 <dmartinez11@luc.edu>
2020-04-24 09:43:17 -0500

    Create FireInspection.java

---
+++ src/src/Model/FireInspection.java
@@ -0,0 +1,33 @@
+package src.Model;
+
+public class FireInspection extends Inspection
+{
+  public FireInspection() {
+    super();
+   }
+   
+  public FireInspection(InspectionType inspectionType, Interval interval)
+  {
+    super(inspectionType, interval);
+  }
+  
+  public InspectionType getInspectionType()
+  {
+    return new Fire();
+  }
+  
+  public void setInspectionType(InspectionType inspectionType)
+  {
+    this.inspectionType = inspectionType;
+  }
+  
+  public Interval getInterval()
+  {
+    return interval;
+  }
+  
+  public void setInterval(Interval interval)
+  {
+    this.interval = interval;
+  }
+}
